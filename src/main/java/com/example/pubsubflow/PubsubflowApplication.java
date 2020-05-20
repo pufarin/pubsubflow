cc43767713e59f6c2d0d67e126c56f1511c32781
gabrielk <mail@gabrielkovacs.net>
2020-05-20 11:22:24 +0200

    first commit

---
+++ src/main/java/com/example/pubsubflow/PubsubflowApplication.java
@@ -0,0 +1,13 @@
+package com.example.pubsubflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PubsubflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PubsubflowApplication.class, args);
+	}
+
+}
